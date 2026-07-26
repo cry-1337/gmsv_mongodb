58dad03cf08cd6928e6f9a56a44a1c0018cd1bf8
cry-1337 <146534003+cry-1337@users.noreply.github.com>
2026-07-27 02:26:42 +0700

    ci/cd(linux): fixed

--- .github/vcpkg-triplets/x64-linux-gmod.cmake
+++ .github/vcpkg-triplets/x64-linux-gmod.cmake
@@ -1,6 +1,7 @@
 set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
+set(VCPKG_CMAKE_SYSTEM_NAME Linux)
 
 # Static libs get linked into a shared GMod module, so build them position-independent.
 set(VCPKG_CMAKE_CONFIGURE_OPTIONS "-DCMAKE_POSITION_INDEPENDENT_CODE=ON")
--- .github/vcpkg-triplets/x86-linux-gmod.cmake
+++ .github/vcpkg-triplets/x86-linux-gmod.cmake
@@ -1,6 +1,7 @@
 set(VCPKG_TARGET_ARCHITECTURE x86)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
+set(VCPKG_CMAKE_SYSTEM_NAME Linux)
 
 # 32-bit build on a 64-bit host: compile every dependency with -m32.
 set(VCPKG_C_FLAGS "-m32")
